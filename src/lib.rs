238d0ca499f345fd48e71c9d2fc9358804d5a3e0
Jovansonlee Cesar <ivanceras@gmail.com>
2017-11-20 22:26:06 +0800

    deal with more than 1 ip returned in hostname -i

--- src/lib.rs
+++ src/lib.rs
@@ -8,19 +8,25 @@ pub fn get() -> Option<IpAddr> {
         .expect("failed to execute `hostname`");
 
     let stdout = String::from_utf8(output.stdout).unwrap();
-    let trimmed = stdout.trim();
-    if !stdout.is_empty(){
-        if let Ok(addr) = trimmed.parse::<Ipv4Addr>() {
-            return Some(IpAddr::V4(addr))
+    let ips: Vec<&str> = stdout.trim().split(" ").collect::<Vec<&str>>();
+    let first = ips.first();
+    match first{
+        Some(first) =>  {
+            if !first.is_empty(){
+                if let Ok(addr) = first.parse::<Ipv4Addr>() {
+                    return Some(IpAddr::V4(addr))
+                }
+                else if let Ok(addr) = first.parse::<Ipv6Addr>() {
+                    return Some(IpAddr::V6(addr))
+                }
+                else{
+                    None
+                }
+            }else{
+                None
+            }
         }
-        else if let Ok(addr) = trimmed.parse::<Ipv6Addr>() {
-            return Some(IpAddr::V6(addr))
-        }
-        else{
-            None
-        }
-    }else{
-        None
+        None => None
     }
 
 }
